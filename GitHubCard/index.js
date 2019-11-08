8fb2175d7a54d1b221251080a42887c821189636
Alison Ludick <alison.ludick@gmail.com>
2019-11-08 20:14:41 +0200

    completed usercard project

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,18 +3,14 @@
            https://api.github.com/users/<your name>
 */
 
-function getGithubData() {
-    debugger
     axios.get('https://api.github.com/users/alisonludick')
         .then(response => {
-            document.body.innerText = response.data.location;
+            gitHubCard(response); 
         })
         .catch(error => {
-            document.body.innerText = error.message;
-        });
-}
+            console.log(error);
+        })
 
-getGithubData();
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -37,7 +33,19 @@ getGithubData();
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+axios.get('https://api.github.com/users/alisonludick/followers') 
+        .then(response => {
+          response.data.forEach(user => {
+            axios.get(`https://api.github.com/users/${user.login}`)
+            .then(response => {
+              gitHubCard(response); 
+            })
+          })
+        })
+        .catch(err => {
+          console.log(err); 
+        })
+      
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -59,10 +67,41 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+function gitHubCard (response) {
+  const parentDiv = document.createElement('div'); 
+  const childDiv = document.createElement('div'); 
+  const img = document.createElement('img'); 
+  const h3 = document.createElement('h3'); 
+  const p1 = document.createElement('p'); 
+  const p2 = document.createElement('p'); 
+  const p3 = document.createElement('p'); 
+  const url = document.createElement('a'); 
+  const p4 = document.createElement('p'); 
+  const p5 = document.createElement('p'); 
+  const p6 = document.createElement('p'); 
+
+  img.setAttribute('src', response.data['avatar_url']); 
+  url.setAttribute('href', response.data['html_url']); 
+  h3.textContent = response.data.name; 
+  p1.textContent = response.data.login; 
+  p2.textContent = response.data.location; 
+  p3.textContent = "Profile: "; 
+  url.textContent = response.data.html_url; 
+  p4.textContent = "Followers: " + response.data.followers; 
+  p5.textContent = "Following: " + response.data.following; 
+  p6.textContent = response.data.bio; 
+
+  parentDiv.classList.add('card'); 
+  childDiv.classList.add('card-info');
+  h3.classList.add('name'); 
+  p1.classList.add('username'); 
+  let elements = [h3, p1, p2, p3, p4, p5, p6]
+  elements.forEach(element => childDiv.appendChild(element)); 
+
+  const cardContainer = document.querySelector('.cards'); 
+  cardContainer.appendChild(parentDiv); 
+  parentDiv.appendChild(img); 
+  parentDiv.appendChild(childDiv); 
+  p3.appendChild(url); 
+  return parentDiv; 
+}
